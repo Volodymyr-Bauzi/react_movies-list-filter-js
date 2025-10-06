8a74fc337546da10fb0bd20ec91b62b6bd26981a
Bauzi <volodiasyd@gmail.com>
2025-10-06 11:32:25 +0300

    Solution #3

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export const App = () => {
                 className="input"
                 placeholder="Type search word"
                 value={query}
-                onChange={event => setQuery(event.target.query)}
+                onChange={event => setQuery(event.target.value)}
               />
             </div>
           </div>
